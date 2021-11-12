5d7a0c2f1b18efb9423bc8e8dd50dc73484d33c4
iidzim9 <ikramid99@gmail.com>
2021-11-12 21:26:31 +0100

    02-03 done /* access file by relative path */

--- frontendmasters/test.py
+++ frontendmasters/test.py
@@ -10,39 +10,39 @@ import requests
 #################################
 
 # #? solution 1: Fetching the Github API
-# def response_with_most_stars():
-#     gh_api_url = “https://api.github.com/search/repositories"
-#     param = { "q": "stars:>50000"}
-#     response = requests.get(gh_api_url, params=param)
-#     print(response.text)
-
-#? solution 2: Searching the Github API
-
-def create_query(language, min_stars=50000):
-    query = f"stars:>{min_stars} "
-    for lang in language:
-        query += f"language:{lang} "
-    return query
-
 def response_with_most_stars():
     gh_api_url = "https://api.github.com/search/repositories"
     param = { "q": "stars:>50000"}
     response = requests.get(gh_api_url, params=param)
-    response_json = response.json()["items"]
-    # print(response_json)
-    return response_json
+    print(response.text)
+
+#? solution 2: Searching the Github API
+
+# def create_query(language, min_stars=50000):
+#     query = f"stars:>{min_stars} "
+#     for lang in language:
+#         query += f"language:{lang} "
+#     return query
+
+# def response_with_most_stars():
+#     gh_api_url = "https://api.github.com/search/repositories"
+#     param = { "q": "stars:>50000"}
+#     response = requests.get(gh_api_url, params=param)
+#     response_json = response.json()["items"]
+#     # print(response_json)
+#     return response_json
 
 
 if __name__ == "__main__":
-    result = response_with_most_stars()
-    languages = ["python", "Javascript", "Ruby"]
-    query = create_query(languages)
-    print("query is ", query)
+    response_with_most_stars()
+    # languages = ["python", "Javascript", "Ruby"]
+    # query = create_query(languages)
+    # print("query is ", query)
     # print(len(result))
-    for res in result:
-        language = res["language"]
-        stars = res["stargazers_count"]
-        name = res["name"]
+    # for res in result:
+    #     language = res["language"]
+    #     stars = res["stargazers_count"]
+    #     name = res["name"]
 
         # print(f"name -> {name}\tlanguage -> {language}\tstars -> {stars}\n")
 
--- module02/ex03/csvreader.py
+++ module02/ex03/csvreader.py
@@ -3,29 +3,71 @@ import csv
 #? opens, reads, and parses a CSV file
 
 class CsvReader():
-    def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
+	def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
+		self.filename = filename
+		self.fd = None
+		self.data = ""
+		self.header = header
+		self.skip_top = skip_top
+		self.skip_bottom = skip_bottom
+		self.sep = sep
+		self.corrupted = False
 
-        pass
+	def __enter__(self):
+		try:
+			self.fd = open(self.filename)
+			self.data = self.fd.read();
+			if self.is_corrupted():
+				self.corrupted = True
+			return self
+		except FileNotFoundError:
+			print("Wrong file or file path")
+			return None
 
-    def __enter__(self):
-        with open(self.filename) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            line_index = 0
-            for row in csv_reader:
-                if line_index == 0:
-                    print(f'column -> {"".join(row)}')
-                    line_index += 1
-                else:
-                    i = 0
-                    while (row[i]):
-                        print(f'row[{i}] -> {row[i]}\n')
-                        i += 1
-        print(f'-------> line_index : {line_index}\n')             
+	def __exit__(self, exception_type, exception_value, traceback):
+		if self.fd:
+			self.fd.close()
 
-    def __exit__(self):
-        pass
+	def is_corrupted(self):
+		data = self.data.split('\n')
+		first_line = self.data[0]
+		for line in data:
+			if len(first_line.split(self.sep)) == len(line.split(self.sep)):
+				return False
+		return True
+
+	def getdata(self):
+		if self.corrupted:
+			# return "the file is corrupted"
+			return None
+		data = self.data.split('\n')
+		if self.header:
+			data = data[1:]
+		if self.skip_top > 0:
+			data = data[self.skip_top - 1:]
+		if self.skip_bottom > 0:
+			data = data[:len(data) - self.skip_bottom]
+		final_data = []
+		for line in data:
+			final_data += line.split(self.sep)
+		return final_data
+
+	def getheader(self):
+		if self.corrupted:
+			# return "the file is corrupted"
+			return None
+		if self.header:
+			result = self.data.split('\n')[0]
+			return (result.split(self.sep))
+
 
 if __name__ == "__main__":
-    test = CsvReader()
+	with CsvReader("good.csv", header=True, skip_top=3, skip_bottom=3) as file:
+		data = file.getdata()
+		header = file.getheader()
 
+	with CsvReader('bad.csv') as file:
+		if file == None:
+			print("File is corrupted")
 
+#? access file by relative path 
