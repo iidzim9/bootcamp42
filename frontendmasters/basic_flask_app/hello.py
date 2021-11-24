e9c2944239940262c129aafccc8f9fd87fe63cf9
iidzim9 <ikramid99@gmail.com>
2021-11-24 20:07:12 +0100

    simple webApp flask

--- frontendmasters/basic_flask_app/hello.py
+++ frontendmasters/basic_flask_app/hello.py
@@ -1,6 +1,17 @@
-from flask import Flask, app
-a = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world!"
+    name = "ikram"
+    return render_template("index.html", name=name)
+
+@app.route("/french")
+def bonjour():
+    return "Bonjour World!"
+
+@app.route("/name/<name>")
+def hello_name(name):
+    return f"hello, {name}"
+
+#? FLASK_APP=hello.py FLASK_ENV=development python3 -m flask run 
