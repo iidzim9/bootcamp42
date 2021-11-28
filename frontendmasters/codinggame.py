da58c848c7116a7e1d2ccca457c33a91fc67492b
Ikram Idzim <iidzim@e2r6p12.1337.ma>
2021-11-28 20:54:36 +0100

    upvote condition x-=1

--- frontendmasters/codinggame.py
+++ frontendmasters/codinggame.py
@@ -2,8 +2,6 @@ import sys
 import math
 
 # s = input().lower().replace(" ", "")
-# # Write an answer using print
-# # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 # d = {}
 # for c in s:
 #     if c in d:
@@ -12,7 +10,7 @@ import math
 #         d[c] = 1
 # print(" ".join([f"{c}:{d[c]}" for c in d]))
 
-########################################################
+#*#######################################################
 
 # lm,ls = map(int,input().split(":"))
 # l=lm*60+ls
@@ -20,20 +18,20 @@ import math
 # p=pm*60+ps
 # t=int(p/l*15)
 
-########################################################
+#*#######################################################
 
 # print("*****************")
 # print(f'*{"-"*t:<15}*')
 # print("*****************")
 
-########################################################
+#*#######################################################
 
 # p=input
 # print(int(p())+int(p())*(int(p())-1))
 # #or
 # print(int(input())+int(input())*(int(input())-1))
 
-########################################################
+#*#######################################################
 
 #largest prime number 
 # def is_prime(n):
@@ -51,7 +49,7 @@ import math
 #             p = m
 # print(p)
 
-########################################################
+#*#######################################################
 
 # # 0 - 99 remote up and down button
 # n = int(input())
@@ -60,7 +58,7 @@ import math
 #     print(min(abs(a-b), abs(100+a-b), abs(100+b-a)))
 
 
-########################################################
+#*#######################################################
 
 # n = int(input())
 # d = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'q':16, 'r':17, 's':18, 'u':19, 'v':20, 'w':21, 'x':22, 'y':23, 'z':24}
@@ -73,44 +71,46 @@ import math
 #         c += str(d.get(l)) + "\n"
 # print(c)
 
-########################################################
+# # ? code after optimisation
+# for i in [{'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'q':16, 'r':17, 's':18, 'u':19, 'v':20, 'w':21, 'x':22, 'y':23, 'z':24}.get(input().lower()) for i in range(int(input()))]:
+#     if i:
+#         print(i)
 
-e_prices = input()
-i_prices = input()
-ep = e_prices.split(' ')
-ip = i_prices.split(' ')
-esum = isum = 0
-for i in range(0, len(ep)):
-    esum += int(ep[i])
-for i in range(0, len(ip)):
-    isum += int(ip[i])
-x = str(int(esum / isum * 100)) + "%"
-print(x)
+#*#######################################################
 
-########################################################
+# e_prices = input()
+# i_prices = input()
+# ep = e_prices.split(' ')
+# ip = i_prices.split(' ')
+# esum = isum = 0
+# for i in range(0, len(ep)):
+#     esum += int(ep[i])
+# for i in range(0, len(ip)):
+#     isum += int(ip[i])
+# x = str(int(esum / isum * 100)) + "%"
+# print(x)
 
-#mycode
-import sys
-import math
+#*#######################################################
+
+#?mycode
 t=input()
 s=input()
 a=input()
 x=r=0
 tt=t.split(' ')
-if len(tt) >= 2:
+if len(tt)>1:
     for tx in tt:
         if len(tx)>=2:
             r+=1
     if r==len(tt):
         x+=1
 ss=s.split(' ')
-if len(ss)>=10 and ss[0][0].isupper():
-    x+=1
-if a.find("BOT") == -1:
-    x+=1
+if len(ss)>9 and ss[0].isupper():x+=1
+if a.find("BOT") == -1:x+=1
+#add upvote condition #!
 print(x)
 
-#Maexel's code
+#?Maexel's code
 t,s,a=open(0)
 c=0
 if len(t.split())>1 and all(len(p)>2for p in t.split()):c+=1
